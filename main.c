a970d2ae67a20518587f0e579e9326de90c77aa5
Amanda Trevisan <amanda.2015@alunos.utfpr.edu.br>
2017-05-30 19:46:21 -0300

    update

--- main.c
+++ main.c
@@ -46,38 +46,75 @@ void printElement(int line, int column) {
   } else if (column == COLUMN - 1 && matrix[line][column - 1] == 0 && matrix[line][column] == 1) {
     printf("\u2502\n");
   } else {
+    if (matrix[line - 1][column] == 1 && matrix[line + 1][column] == 1 && matrix[line][column - 1] == 1  && matrix[line][column + 1] == 1) {
+      printf("\u253C");
+    } else if (matrix[line + 1][column] == 1 && matrix[line][column - 1] == 1 && matrix[line][column + 1] == 1) {
+      printf("\u252C");
+    } else if (matrix[line - 1][column] == 1 && matrix[line][column - 1] == 1 && matrix[line][column + 1] == 1) {
+      printf("\u2534");
+    } else if (matrix[line - 1][column] == 1 && matrix[line + 1][column] == 1 && matrix[line][column + 1] == 1) {
+      printf("\u251C");
+    } else if (matrix[line - 1][column] == 1 && matrix[line + 1][column] == 1 && matrix[line][column - 1] == 1) {
+      printf("\u2524");
+    } else if (matrix[line + 1][column] == 1 && matrix[line][column + 1] == 1) {
+      printf("\u250C");
+    } else if (matrix[line - 1][column] == 1 && matrix[line][column + 1] == 1) {
+      printf("\u2514");
+    } else if (matrix[line - 1][column] == 1 && matrix[line + 1][column] == 1 && matrix[line][column - 1] == 1  && matrix[line][column + 1] == 1) {
+      printf("\u252C");
+    
     printf(" ");
   }
   
 }
 
+//imprimir matrix do jogo
 void printMatrix () {
-  for (int line = 0; line < LINE; line++) {
-    for (int column = 0; column < COLUMN; column++) {
-      //printf("%d", matrix[line][column]);
-      printElement(line, column);
+  for (int line = 0; line < LINE; line++) {  //vare toda as linhas (vai do 0 até LINE -1)
+    for (int column = 0; column < COLUMN; column++) { //vare toda as colunas (vai do 0 até COLUMN -1)
+      //printf("%d", matrix[line][column]); //debug
+      printElement(line, column); //formatar com os caracteres especiais definidos lá em cima
     }
-    //printf("\n");
+    //printf("\n"); //debug
+  }
+}
+
+void generateMaze (int line, int column) { //https://en.wikipedia.org/wiki/Maze_generation_algorithm#Python_code_examples
+  int destinations[4] = {0,0,0,0};
+  int sum = 0;
+  if (line - 1 >= 0 && matrix[line - 1][column] == 10) {
+    destinations[0] = 1;
+    sum++;
+  }
+  if (line + 1 < LINE && matrix[line + 1][column] == 10) {
+    destinations[1] = 1;
+    sum++;
+  }
+  if (column - 1 >= 0 && matrix[line][column - 1] == 10) {
+    destinations[2] = 1;
+    sum++;
+  }
+  if (column + 1 < COLUMN && matrix[line][column + 1] == 10) {
+    destinations[3] = 1;
+    sum++;
   }
 }
 
 int main () {
-  int random = 0;
-  srand(time(NULL));
-  //Zerando a matriz
-  for (int line = 0; line < LINE; line++) {
+  int random = 0; //random é uma variavel que será sorteada para definir a entrada
+  srand(time(NULL)); //essa é a "semente" de gerador aleatorio, (semente é o numero para geração dos proximos aleatórios.)
+  for (int line = 0; line < LINE; line++) {  //Zerando a matriz
     for (int column = 0; column < COLUMN; column++) {
-      if (line == 0 || column == 0 || line == LINE - 1 || column == COLUMN - 1) {
+      if (line == 0 || column == 0 || line == LINE - 1 || column == COLUMN - 1) { //isso é o que gera todas as bordas, desenhando.
 	matrix[line][column] = 1;
       } else {
-	matrix[line][column] = 0;
+	matrix[line][column] = 10; //é o que deixa tudo dentro do labirinto vazio, 10 espaçõ não inicializado
       }
     }
   }
-  //Gera um numero aleatorio para a entrada entre 1 e LINE - 1
-  random = (rand() % (LINE - 2)) + 1;
+  random = (rand() % (LINE - 2)) + 1; //Gera um numero aleatorio para a entrada entre 1 e LINE - 1
 
-  matrix[random][0] = 0;
-  printMatrix();
+  matrix[random][0] = 0; //abrir entrada, pois 0 é campo vazio.
+  printMatrix(); //imprimir o labirinto
   return 0;
 }
